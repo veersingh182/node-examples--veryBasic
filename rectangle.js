699de3b3ac3d84e985df9a68c46a9467f3b0c9ef
Virendra Saini <veerugsaini@gmail.com>
2021-10-12 13:19:01 +0530

    Updated rectangle.js module

--- rectangle.js
+++ rectangle.js
@@ -1,4 +1,4 @@
-module.exports = (x,y,callback) => {
+module.exports  = (x,y,callback) => {
     if (x<=0 || y<=0) {
         setTimeout(()=>
         callback(new Error("Rectangle dimensions are not valid."),null),
